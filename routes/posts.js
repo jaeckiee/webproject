8d88d799ab66400bb82f01529bafe32978a3f62d
­조용기(학부/공과대학 컴퓨터과학) <okcho9807@yonsei.ac.kr>
2021-01-15 15:46:33 +0000

    일부 글 작성 오류 수정

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ import util from "../util.js";
 // Index
 router.get('/', function(req, res) {
     Post.find({})
-    // .populate('author')
+    .populate('author')	// relationship 항목의 값을 생성시켜줌
     .sort('-createdAt')
     .exec(function(err, posts) {
         if (err) return res.json(err);
@@ -23,11 +23,12 @@ router.get('/new', function(req, res) {
 
 // Create
 router.post('/', function(req, res) {
-    // req.body.author = req.user._id;
+    req.body.author = req.user._id;
     Post.create(req.body, function(err, post) {
         if (err) {
             req.flash('post', req.body);
             req.flash('errors', util.parseError(err));
+			// console.log("씨발 뭔데: "+err);
             return res.redirect('/posts/new');
         }
         res.redirect('/posts');
@@ -37,7 +38,7 @@ router.post('/', function(req, res) {
 // Show
 router.get('/:id', function(req, res) {
     Post.findOne({ _id: req.params.id })
-        // .populate('author')
+        .populate('author')
         .exec(function(err, post) {
             if (err) return res.json(err);
             res.render('posts/show', { post: post });
@@ -75,6 +76,7 @@ router.put('/:id', function(req, res) {
 
 // destroy
 router.delete('/:id', function(req, res) {
+	console.log("이거 들오냐??????????????????");
     Post.deleteOne({_id: req.params.id}, function(err) {
         if (err) return res.json(err);
         res.redirect('/posts');
