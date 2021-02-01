761b106d3de736cb53db15918383183507d64da6
jaeckiee <jaechan11@gmail.com>
2021-02-01 08:43:40 +0000

    autocomplete - modified

--- public/js/script.js
+++ public/js/script.js
@@ -37,57 +37,59 @@ $(function() {
             }
         });
     }
-// Autocomplete
-	$.ajax({
-		type : 'get',
-		url : "/posts/search/autocomplete",
-		dataType : "json",
-		success : function(data) {
-			var source = $.map(data, function(item) {
-				var consonant = "";
-				var conplusvo = "";
-				var full = Hangul.disassemble(item).join("").replace(/ /gi, "");
-				Hangul.d(item, true).forEach(function(strItem, index) {
-					if(strItem[0] != " "){
-						consonant += strItem[0];
-					}
-				});
-				Hangul.d(item, true).forEach(function(strItem, index) {
-					if(strItem[0] != " "){
-						if(strItem.length < 3){
-							conplusvo += Hangul.assemble(strItem);
+	// Autocomplete
+	$("#search").mousedown(function() {
+		$.ajax({
+			type : 'get',
+			url : "/posts/search/autocomplete",
+			dataType : "json",
+			success : function(data) {
+				var source = $.map(data, function(item) {
+					var consonant = "";
+					var conplusvo = "";
+					var full = Hangul.disassemble(item).join("").replace(/ /gi, "");
+					Hangul.d(item, true).forEach(function(strItem, index) {
+						if(strItem[0] != " "){
+							consonant += strItem[0];
 						}
-						else{
-							conplusvo += Hangul.assemble(strItem.slice(0, 2));
+					});
+					Hangul.d(item, true).forEach(function(strItem, index) {
+						if(strItem[0] != " "){
+							if(strItem.length < 3){
+								conplusvo += Hangul.assemble(strItem);
+							}
+							else{
+								conplusvo += Hangul.assemble(strItem.slice(0, 2));
+							}
 						}
+					});
+					// consonant: 한글같은 경우 자음만 모은것 exㅎㄴㄱㅌㅇㄱㅇㅈㅇㅁㅁㅇㄱ
+					// conplusvo: 한글같은 경우 받침 제외 자음과 모음만 모은것 ex 하그가으겨우
+					// full: 한글같은 경우 모음, 자음, 받침들이 모두 분리 된 후 다시 합쳐지고 띄어 쓴 부분은 없앤다. ex ㅎㅏㄴㄱㅡㄹ
+					return {
+						label : (item).replace(/ /gi, "")+ "|" + consonant + "|" + full + "|" + conplusvo,
+						value : item
 					}
-				});
-				// consonant: 한글같은 경우 자음만 모은것 exㅎㄴㄱㅌㅇㄱㅇㅈㅇㅁㅁㅇㄱ
-				// conplusvo: 한글같은 경우 받침 제외 자음과 모음만 모은것 ex 하그가으겨우
-				// full: 한글같은 경우 모음, 자음, 받침들이 모두 분리 된 후 다시 합쳐지고 띄어 쓴 부분은 없앤다. ex ㅎㅏㄴㄱㅡㄹ
-				return {
-					label : (item).replace(/ /gi, "")+ "|" + consonant + "|" + full + "|" + conplusvo,
-					value : item
-				}
-			})
-			$("#search").autocomplete({
-  				source: source,
-				delay: 300,
-				focus: function(event, ui){
-					return false;
-				},
-				open: function() {
-					$("ul.ui-menu").width( $(this).innerWidth() );
-					$('ul li div').addClass("list-group-item");
-				},
-				minLength: 1
-			}).autocomplete("instance")._renderItem = function(ul, item) {  
-				return $("<li>")
-				.append( "<div>" + item.value + "</div>" )
-				.appendTo(ul);
-			};
-		}
-	});
+				})
+				$("#search").autocomplete({
+  					source: source,
+					delay: 300,
+					focus: function(event, ui){
+						return false;
+					},
+					open: function() {
+						$("ul.ui-menu").width( $(this).innerWidth() );
+						$('ul li div').addClass("list-group-item");
+					},
+					minLength: 1
+				}).autocomplete("instance")._renderItem = function(ul, item) {  
+					return $("<li>")
+					.append( "<div>" + item.value + "</div>" )
+					.appendTo(ul);
+				};
+			}
+		});
+	})
     convertDate();
     convertDateTime();
 });
